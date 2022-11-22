00fb181caaa4d78d7be1f7dfac7c02c7acbcb351
Roman Zabolotin <rzabolotin@gmail.com>
2022-11-22 20:09:30 +0600

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -23,12 +23,12 @@ variable "instance_type" {
 variable "alert_email_id" {
   description = "Email id to send alerts to "
   type        = string
-  default     = "joseph.machado@startdataengineering.com"
+  default     = "rzabolotin@gmail.com"
 }
 
 ## Your repository url
 variable "repo_url" {
   description = "Repository url to clone into production machine"
   type        = string
-  default     = "https://github.com/josephmachado/data_engineering_project_template.git"
+  default     = "https://github.com/rzabolotin/dummy_de_project_from_template.git"
 }
